169ca5ff6e3dfe0e9bbf48d5d366b6c18a94f9c9
msecn <msecn@iscte-iul.pt>
2021-11-26 17:35:34 +0000

    Projeto com nome do projeto,cards e boards

--- ProjetoES_/src/main/java/pt/iscte_iul/ProjetoES_/App.java
+++ ProjetoES_/src/main/java/pt/iscte_iul/ProjetoES_/App.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.trello4j.Trello;
 import org.trello4j.TrelloImpl;
 import org.trello4j.model.Board;
+import org.trello4j.model.Card;
 
 
 
@@ -12,24 +13,25 @@ import org.trello4j.model.Board;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        String trelloKey = "e9ce836fb30a14586c6b47cb5de0c08f";
-        String trelloAcessToken="32123daef3beb3b117540a482a6a889bf57b4586963251423a58ec8deee0d74";
-        Trello trelloApi= new TrelloImpl(trelloKey,trelloAcessToken);
-        Board board;
-        String nome_utilizador="margaridaneves3";
-        List<Board> member=trelloApi.getBoardsByMember(nome_utilizador);
-        for(Board quadro: member) {
-        	System.out.println(quadro.getName()+ "-" +quadro.getId());
-        	board=trelloApi.getBoard(quadro.getId());
-        	List<List> lists=board.fetchLists();
-        	for(TList lista: lists) {
-        		System.out.println(lista.getName()+ "-" +lista.getId()+ "-" +lista.getIdBoard());
+public class App {
+	
+    public static void main( String[] args ){ 	   
+        String trelloKey = "03a00a367901cc8e2cbd3a2e6270413c";
+        String trelloAcessToken = "6e472de0607c15ac4429f146b7eee03ab057ca592890a435443a72a613e42b2e";
+        Trello trelloApi = new TrelloImpl(trelloKey,trelloAcessToken);
+        String nome_utilizador = "pedroduartecarrondoarsenio";
+        List<Board> boards = trelloApi.getBoardsByMember(nome_utilizador);
+        Board board = boards.get(0);
+        String IdOrg = board.getIdOrganization();
+        System.out.println("Nome do Projeto: "+ trelloApi.getOrganization(IdOrg).getDisplayName());
+        List<Card> cards;
+        	System.out.println("Nome do Board: "+ board.getName());
+        	cards = trelloApi.getCardsByMember(nome_utilizador);
+        	List<org.trello4j.model.List> list=trelloApi.getListByBoard(board.getId());
+        	//System.out.println(cards);
+        	System.out.println("Lista dos Cards:");
+        	for(org.trello4j.model.List lista: list) {
+        		System.out.println("\t"+lista.getName());
         	}
-        }
     }
 }
