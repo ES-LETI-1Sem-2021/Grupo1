2ad49ec17cfdd9a211d043f8489aef7eb1b51fc0
PedroArsenio33 <pdcao1@iscte-iul.pt>
2021-12-03 21:11:13 +0000

    Added getTags() method.

--- ProjetoES_/src/main/java/pt/iscte_iul/ProjetoES_/GithubMethods.java
+++ ProjetoES_/src/main/java/pt/iscte_iul/ProjetoES_/GithubMethods.java
@@ -2,23 +2,36 @@ package pt.iscte_iul.ProjetoES_;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.io.IOUtils;
 import org.kohsuke.github.*;
 
 public class GithubMethods {
-	
+
 	private String githubKey;
 	private GitHub github;
 	private GHRepository repo;
-	
+
 	public GithubMethods() throws IOException {
-		githubKey = "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";
+		githubKey = "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";
 		github = GitHub.connectUsingOAuth(githubKey);
 		repo = github.getRepository("PedroArsenio33/ES-LETI-1Sem-2021-Grupo1");
 	}
-	
+
 	public String getReadme() throws IOException {
 		return IOUtils.toString(repo.getFileContent("README.md").read(), StandardCharsets.UTF_8);
 	}
 	
+	public List<String> getTags() throws IOException {
+		List<String> allTags = new ArrayList<String>(); 
+		List<GHTag> tags = repo.listTags().toList();
+		for (GHTag tag : tags) {
+			String tagName = tag.getName();
+			String tagDate = tag.getCommit().getCommitDate().toString();
+			allTags.add("Nome: " + tagName.concat("  Data: " + tagDate));
+		}
+		return allTags;
+	}
 }
--- ProjetoES_/src/main/java/pt/iscte_iul/ProjetoES_/Main.java
+++ ProjetoES_/src/main/java/pt/iscte_iul/ProjetoES_/Main.java
@@ -10,21 +10,25 @@ public class Main {
 		
 		System.out.println("\tConteúdo do README.md:\n" + github.getReadme());
 		
+		System.out.println("\tTags:");
+		for (String tags : github.getTags()) {
+        	System.out.println(tags);
+        }
+		
 		System.out.println("\tNome do projeto:\n" + trello.getProjectID());
 		
 		System.out.println("\tUsername dos membros:");
-		for(String names : trello.getProjectMembers()) {
+		for (String names : trello.getProjectMembers()) {
         	System.out.println(names);
         }
 		
 		System.out.println("\tData do início do projeto:\n" + trello.getProjectStartDate());
-		
-		for(String desc : trello.getMeetingDescriptions()) {
+		for (String desc : trello.getMeetingDescriptions()) {
         	System.out.println("\t" + desc);
         }
 		
 		System.out.println("\tItems em cada Sprint:");
-		for(String items : trello.getItemsDoneEachSprint()) {
+		for (String items : trello.getItemsDoneEachSprint()) {
         	System.out.println(items);
         }
 	}
